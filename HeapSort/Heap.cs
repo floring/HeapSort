712ba4079983d5d6191d04cddc69a3997bc27d93
floring <floring@users.noreply.github.com>
2015-05-26 12:26:45 +0300

    Implement heap sort.

---
+++ HeapSort/Heap.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort
+{
+    public class Heap
+    {
+        private int[] _array;
+
+        public Heap(int[] array)
+        {
+            _array = array;
+        }
+
+        private void BuildMaxHeap()
+        {
+            for (int i = _array.Length / 2; i >= 0; i--)
+            {
+                MaxHeapify(_array.Length, i);
+            }
+        }
+
+        private void MaxHeapify(int length, int i)
+        {
+            int l = Left(i);
+            int r = Right(i);
+            int largest = i;
+            if (l < length && _array[i] < _array[l])
+            {
+                largest = l;
+            }
+            if (r < length && _array[largest] < _array[r])
+            {
+                largest = r;
+            }
+            if (i != largest)
+            {
+                Swap(i, largest);
+                MaxHeapify(length, largest);
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            int tmp = _array[i];
+            _array[i] = _array[j];
+            _array[j] = tmp;
+        }
+
+        private int Left(int i)
+        {
+            return 2 * i + 1;
+        }
+
+        private int Right(int i)
+        {
+            return 2 * i + 2;
+        }
+
+        public int[] Sort()
+        {
+            BuildMaxHeap();
+            for (int i = _array.Length - 1; i > 0; i--)
+            {
+                Swap(i, 0);
+                MaxHeapify(i, 0);
+            }
+            return _array;
+        }
+    }
+}
---
+++ HeapSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputArray = new int[] { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+            Console.WriteLine("Input not sorted array:");
+            PrettyPrint(inputArray);
+
+            var heap = new Heap(inputArray);
+            var resultArray = heap.Sort();
+
+            Console.WriteLine("Output sorted array:");
+            PrettyPrint(resultArray);
+
+            
+        }
+
+        static void PrettyPrint(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                Console.Write(String.Format("{0} ", array[i]));
+            Console.WriteLine();
+        }
+    }
+}
